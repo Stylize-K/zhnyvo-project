ce635156c12676db22a48e35e46be87116992216
yanikr <rihter.yan@gmail.com>
2024-01-21 03:51:42 +0200

    added pogin page no functionality basic design

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./utils/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
