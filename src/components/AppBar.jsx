ede951327ce8b0d05d8a51a85e39477da388ceb2
yanikr <rihter.yan@gmail.com>
2024-04-21 02:28:40 +0300

    fixed auth logic
    fixed issues with tokens, added refresh token, added logout, conditional render in menu, routes,

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,14 +1,27 @@
 import {
   AppBar,
   Box,
+  Button,
   Container,
   IconButton,
   Toolbar,
-  Typography,
 } from "@mui/material";
+
 import MenuIcon from "@mui/icons-material/Menu";
+import { Link as RouterLink } from "react-router-dom";
 import KeyboardBackspaceOutlinedIcon from "@mui/icons-material/KeyboardBackspaceOutlined";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "../redux/auth/selectors";
+import { useEffect } from "react";
+import authOperations from "../redux/auth/authOperations";
 export const Appbar = ({ toggleMenu, isMenuOpen }) => {
+  const user = useSelector(selectUser);
+  console.log(user);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(authOperations.getPersonalInfo());
+  }, [dispatch]);
   return (
     <>
       <Container maxWidth="xs">
@@ -29,7 +42,13 @@ export const Appbar = ({ toggleMenu, isMenuOpen }) => {
               >
                 {isMenuOpen ? <KeyboardBackspaceOutlinedIcon /> : <MenuIcon />}
               </IconButton>
-              <Typography>Zhnyvo logo</Typography>
+              <Button
+                color="neutralVariant"
+                component={RouterLink}
+                to={user.isAuthenticated ? "/" : "/signup"}
+              >
+                Zhnyvo logo
+              </Button>
             </Toolbar>
           </AppBar>
         </Box>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,28 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { authReducer } from "./auth/authSlice";
 
+// const loadTokenFromLocalStorage = () => {
+//   try {
+//     const token = localStorage.getItem("accessToken");
+//     if (token) {
+//       return token;
+//     }
+//   } catch (error) {
+//     console.error("Error loading token from local storage:", error);
+//   }
+
+//   // If token doesn't exist or error occurs, return null
+//   return null;
+// };
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+  // preloadedState: {
+  //   // Load token from local storage and set it in the initial state
+  //   auth: {
+  //     token: loadTokenFromLocalStorage(),
+  //   },
+  // },
 });
